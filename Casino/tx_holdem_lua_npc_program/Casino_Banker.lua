177f31f8655366b7fa36a3d94b1c2b8691369785
Sean K. Murphy <laugherx88@gmail.com>
2019-03-11 08:58:23 -0500

    Added Platinum-Credit Conversion Transaction to Casino Banker

--- Casino/tx_holdem_lua_npc_program/Casino_Banker.lua
+++ Casino/tx_holdem_lua_npc_program/Casino_Banker.lua
@@ -1,10 +1,7 @@
 function event_say(e)
 
 	if (e.message:findi("hail")) then
-		e.other:Message(315,"Hail and well met mighty adventurer!  If you would like me to [deal] you in and play some [holdem], just let me know.  If you would like to cash [out] or know how much [credit] you have, I can also help you with that.");
-	end
-	if (e.message:findi("deal")) then
-		e.other:Message(315,"To buy a seat at the table, you will need a balance of at least 200 platinum.  Please note I only accept platinum.");
+		e.other:Message(315,"Hail and well met mighty adventurer!  If you would like to exchange platinum for credit, cash [out], or know how much [credit] you have, I can help you with that.");
 	end
 	if (e.message:findi("out")) then
 		$cashout == eq.get_data(e.other:GetCleanName() .. "_Casino")
@@ -13,4 +10,11 @@ function event_say(e)
 	if (e.message:findi("credit")) then
 		e.other:Message(315,"Currently your balance is " eq.get_data(e.other:GetCleanName() .. "_Casino");
 	end
+end
+
+function event_handin(e)
+
+eq.set_data(e.other:GetCleanName() .. "_Casino") + e.other:GetPlatinum
+e.other:Message(315,"Your platinum has been credited to your account.  Your current balance is: " eq.get_data(e.other:GetCleanName() .. "_Casino");
+
 end
